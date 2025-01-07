510ce4ab02a9a62c82b73a3a69e874694afec8b0
ParmidaAzhir <140430728+ParmidaAzhir@users.noreply.github.com>
2025-01-07 21:18:08 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+   link.addEventListener('click', function(e) {
+      e.preventDefault();
+      let target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+         window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+         });
+      }
+   });
+})
